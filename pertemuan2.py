8333e068f371792db9312292a0e7a583cc7d3553
Hisyam-14 <fadhiilah3837@gmail.com>
2023-09-23 13:29:41 +0800

    push ke 2

---
+++ pertemuan2.py
@@ -0,0 +1,60 @@
+#Variabe
+# a = 10
+# print(a)
+# b = "Saya suka makan"
+# print(b)
+# print(type(a), type(b))
+# c = str(a)
+# print(c, type(c))
+# d = int(a)
+# print(d, type(a))
+
+#Input
+# nama = input("Masukkan nama: ")
+# nim = int(input("Masukkan NIM: "))
+# # print(nama,",",nim)
+# # print(type(nim))
+# print("==========================")
+# print("Nama : ", nama)
+# print("NIM  : ", nim)
+# print("==========================")
+# print(f"Nama saya {nama}. NIM saya {nim}.")
+
+#Operator
+# a = int(input("Masukkan Ankga 1 = "))
+# b = int(input("Masukkan Angak 2 = "))
+# kali = a * b
+# tambah = a + b
+# bagi = a / b
+# kurang = a - b
+# modulus = a % b
+
+# print(kali, tambah, bagi, kurang, modulus)
+# a *= b
+# print("a")
+
+# a = int(input ("Maukkan Angka : "))
+# if(a == 100):
+#     print("Variabel a adalah 100")
+# else:
+#     print("Variabel a lain 100")
+
+# b = input("Masukkan kata - kata : ")
+# if(b == "Hujan"):
+#     print("Basah")
+# else:
+#     print("Kering")
+
+#Masih error
+# print("PROGRAM MENGHITUNG VOLUME KUBUS\n")
+# sisi = int(input("Masukkan sisi kubus: "))
+# volume = sisi * sisi * sisi
+# print(f"Volume kubus adalah: {volume}")
+
+print("PROGRAM MENGHITUNG KELILING LINGKARAN\n")
+
+diameter = int(input("Masukkan diameter lingkaran : "))
+phi = 3.14
+KelilingLingkaran = 2 * phi * diameter
+
+print("Keliling lingkaran adalah : ", KelilingLingkaran)
